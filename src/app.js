80991c1918296107929a0d2308e8a89d149d4639
expeditus303 <ricardotonietto@gmail.com>
2023-01-12 17:27:29 -0300

    participants post

--- src/app.js
+++ src/app.js
@@ -22,7 +22,44 @@ let db;
 try {
   await mongoClient.connect();
   db = mongoClient.db();
-  console.log('Data base is connected')
+  console.log("Data base is connected");
 } catch (err) {
   console.log("Data base is not connected");
 }
+
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  res.send(name);
+
+  try {
+    await db.collection("participants").insertOne({
+      name: name,
+      lastStatus: Date.now(),
+    });
+    await db.collection("messages").insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: "HH:MM:SS",
+    });
+  } catch (err) {}
+});
+
+app.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection("participants").find().toArray();
+    res.send(participants);
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/messages", async (req, res) => {
+    try {
+      const messages = await db.collection("messages").find().toArray();
+      res.send(messages);
+    } catch (err) {
+      res.status(500).send("Internal Server Error");
+    }
+  });
