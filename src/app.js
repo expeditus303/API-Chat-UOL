93c1efbd4b11b11a32697b05c1742bbfa06c01c4
expeditus303 <ricardotonietto@gmail.com>
2023-01-12 16:24:45 -0300

    data base connected

--- src/app.js
+++ src/app.js
@@ -1,22 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import { MongoClient } from "mongodb"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { MongoClient } from "mongodb";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT
-const DATABASE_URL = process.env.DATABASE_URL
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}`)
-})
+  console.log(`Server listening on PORT ${PORT}`);
+});
 
-const mongoClient = new MongoClient(DATABASE_URL)
+const mongoClient = new MongoClient(DATABASE_URL);
 let db;
 
-mongoClient.connect()
+try {
+  await mongoClient.connect();
+  db = mongoClient.db();
+  console.log('Data base is connected')
+} catch (err) {
+  console.log("Data base is not connected");
+}
