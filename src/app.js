54c20e964425904ea416f0009045748c7d05ed6e
expeditus303 <ricardotonietto@gmail.com>
2023-04-12 16:12:33 -0300

    feat: database config

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
+
+
---
+++ src/config/database.connection.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+try {
+  await mongoClient.connect();
+  console.log("Connected to database");
+} catch (err) {
+  console.log(err);
+}
+
+const db = mongoClient.db;
+const participantsCollection = db.collection("participants");
+const messagesCollection = db.collection("messages");
+
+export default { participantsCollection, messagesCollection };
