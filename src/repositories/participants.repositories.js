87e2fbbbc00ae1da4cde3284eed92e30de084a66
expeditus303 <ricardotonietto@gmail.com>
2023-04-14 18:16:48 -0300

    fix: async function participant repo

--- src/repositories/participants.repositories.js
+++ src/repositories/participants.repositories.js
@@ -4,9 +4,9 @@ function findByName({ name }) {
   return db.participantsCollection.findOne({ name });
 }
 
-function create(participant, message) {
-    db.participantsCollection.insertOne(participant)
-    db.messagesCollection.insertOne(message)
+async function create(participant, message) {
+    await db.participantsCollection.insertOne(participant)
+    await db.messagesCollection.insertOne(message)
 }
 
 function getAll() {
